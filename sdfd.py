d7bed1961a09143811b00a3886fc0dc9541cc073
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2023-09-11 14:32:55 +0530

    Update sdfd.py

--- sdfd.py
+++ sdfd.py
@@ -1,7 +1,9 @@
-a = "NITHIN"
-b = ""
-for i in a:
-    if i.isalpha():
-        b+=i.lower()
-print(b)
+
+def check(a):
+    b = ""
+    for i in a:
+        if i.isalpha():
+            b+=i.lower()
+    return b
+print(check("NITHIN"))
 
