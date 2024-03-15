6fe1ca91d75c8f2f81dfaffe92d6e4fb7b7d448d
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2024-03-15 18:29:45 +0530

    Update ama.py

--- ama.py
+++ ama.py
@@ -1,10 +1,15 @@
 import pandas as pd
-df = pd.read_csv("https://github.com/nithi2003/External_Training/blob/main/hello.csv")
+import requests
+from io import StringIO
+url = "https://raw.githubusercontent.com/nithi2003/External_Training/main/hello.csv"
+response = requests.get(url)
+df = pd.read_csv(StringIO(response.text))
 cols = df.iloc[:,:]<1
 df2 = df[cols]*100
 df2 = df2[cols].astype(int)
 df = df2
-if df:
-  print("TRUE")
+if df is not None:
+    print("TRUE")
+    print(df)
 else:
-  print("False")
+    print("False")
