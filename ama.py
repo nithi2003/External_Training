727d71a8d0baf9ea5703756898618821a2223352
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2024-03-15 20:50:07 +0530

    Update ama.py

--- ama.py
+++ ama.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from io import StringIO
+import matplotlib.pyplot as plt
 url = "https://raw.githubusercontent.com/nithi2003/External_Training/main/hello.csv"
 response = requests.get(url)
 df = pd.read_csv(StringIO(response.text))
@@ -8,8 +9,11 @@ cols = df.iloc[:,:]<1
 df2 = df[cols]*100
 df2 = df2[cols].astype(int)
 df = df2
-if df is not None:
-    print("TRUE")
-    print(df)
-else:
-    print("False")
+plt.figure(figsize=(10, 6))
+plt.plot(df["0.234"],df["0.567"])
+plt.title('Data Plot')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig('plot.png')  # Save the plot as a PNG file
+plt.close()
+print("Plot saved as 'plot.png'")
