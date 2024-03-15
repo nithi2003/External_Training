744b4dffd4cc904999eff4c5f338396b86e341c4
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2024-03-15 18:09:53 +0530

    Update ama.py

--- ama.py
+++ ama.py
@@ -4,4 +4,7 @@ cols = df.iloc[:,:]<1
 df2 = df[cols]*100
 df2 = df2[cols].astype(int)
 df = df2
-df.to_csv("C:\ProgramData\Jenkins\.jenkins\workspace\hello.csv",index=False)
+if df:
+  print("TRUE")
+else:
+  print("False")
