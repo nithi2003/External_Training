939b2e08043339ebbd8f9c8140e1c553e4be1cd6
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2024-03-15 18:12:33 +0530

    Update ama.py

--- ama.py
+++ ama.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("C:\ProgramData\Jenkins\.jenkins\workspace\hello.csv")
+df = pd.read_csv("https://github.com/nithi2003/External_Training/blob/main/hello.csv")
 cols = df.iloc[:,:]<1
 df2 = df[cols]*100
 df2 = df2[cols].astype(int)
